208f295cbef8f649962319b1d836a7033190f75a
Heiner Giefers <hgiefers@users.noreply.github.com>
2017-08-17 09:24:20 +0200

    Modification to run script in non-interactive shell

--- capi-flash-script.sh
+++ capi-flash-script.sh
@@ -1,71 +1,71 @@
-#!/bin/bash
-#
-# Copyright 2016, 2017 International Business Machines
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Usage: sudo capi-flash-script.sh <path-to-bit-file>
+#!/bin/bash                                                                                                                
+#                                                                                                                          
+# Copyright 2016, 2017 International Business Machines                                                                     
+#                                                                                                                          
+# Licensed under the Apache License, Version 2.0 (the "License");                                                          
+# you may not use this file except in compliance with the License.                                                         
+# You may obtain a copy of the License at                                                                                  
+#                                                                                                                          
+#     http://www.apache.org/licenses/LICENSE-2.0                                                                           
+#                                                                                                                          
+# Unless required by applicable law or agreed to in writing, software                                                      
+# distributed under the License is distributed on an "AS IS" BASIS,                                                        
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                                 
+# See the License for the specific language governing permissions and                                                      
+# limitations under the License.                                                                                           
+#                                                                                                                          
+# Usage: sudo capi-flash-script.sh <path-to-bit-file>                                                                      
 
 version=1.0
-force=0
+force=0    
 program=`basename "$0"`
-card=-1
+card=-1                
 
 # Print usage message helper function
-function usage() {
+function usage() {                   
   echo "Usage:  sudo ${program} [OPTIONS]"
-  echo "    [-C <card>] card to flash."
+  echo "    [-C <card>] card to flash."   
   echo "    [-f] force execution without asking."
   echo "         warning: use with care e.g. for automation."
-  echo "    [-V] Print program version (${version})"
-  echo "    [-h] Print this help message."
-  echo "    <path-to-bit-file>"
-  echo
+  echo "    [-V] Print program version (${version})"         
+  echo "    [-h] Print this help message."                   
+  echo "    <path-to-bit-file>"                              
+  echo                                                       
   echo "Utility to flash/write bitstreams to CAPI FPGA cards."
   echo "Please ensure that you are using the right bitstream data."
   echo "Using non-functional bitstream data or aborting the process"
   echo "can leave your card in a state where a hardware debugger is"
-  echo "required to make the card usable again."
-  echo
-}
+  echo "required to make the card usable again."                    
+  echo                                                              
+}                                                                   
 
 # Parse any options given on the command line
-while getopts ":C:fVh" opt; do
-  case ${opt} in
-      C)
-      card=$OPTARG
-      ;;
-      f)
-      force=1
-      ;;
-      V)
-      echo "${version}" >&2
-      exit 0
-      ;;
-      h)
-      usage;
-      exit 0
-      ;;
-      \?)
+while getopts ":C:fVh" opt; do               
+  case ${opt} in                             
+      C)                                     
+      card=$OPTARG                           
+      ;;                                     
+      f)                                     
+      force=1                                
+      ;;                                     
+      V)                                     
+      echo "${version}" >&2                  
+      exit 0                                 
+      ;;                                     
+      h)                                     
+      usage;                                 
+      exit 0                                 
+      ;;                                     
+      \?)                                    
       printf "${bold}ERROR:${normal} Invalid option: -${OPTARG}\n" >&2
-      exit 1
-      ;;
-      :)
+      exit 1                                                          
+      ;;                                                              
+      :)                                                              
       printf "${bold}ERROR:${normal} Option -$OPTARG requires an argument.\n" >&2
-      exit 1
-      ;;
-  esac
-done
+      exit 1                                                                     
+      ;;                                                                         
+  esac                                                                           
+done                                                                             
 
 shift $((OPTIND-1))
 # now do something with $@
@@ -73,47 +73,59 @@ shift $((OPTIND-1))
 ulimit -c unlimited
 
 # stop on non-zero response
-set -e
+set -e                     
 
 # output formatting
-bold=$(tput bold)
-normal=$(tput sgr0)
+if tty -s          
+then               
+  bold=$(tput bold)
+  normal=$(tput sgr0)
+else                 
+  bold=              
+  normal=            
+fi                   
 
 # make sure script runs as root
-if [[ $EUID -ne 0 ]]; then
-  printf "${bold}ERROR:${normal} This script must run as root (${EUID})\n"
-  exit 1
-fi
+if [[ $EUID -ne 0 ]]; then     
+  printf "${bold}ERROR:${normal} This script should run as root (${EUID})\n"
+#  exit 1                                                                   
+fi                                                                          
 
 # make sure an input argument is provided
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then                    
   printf "${bold}ERROR:${normal} Input argument missing\n"
   printf "Usage: sudo capi-flash-script <path-to-bit-file>\n"
-  exit 1
-fi
+  exit 1                                                     
+fi                                                           
 
 # make sure the input file exists
-if [[ ! -e $1 ]]; then
+if [[ ! -e $1 ]]; then           
   printf "${bold}ERROR:${normal} $1 not found\n"
-  exit 1
-fi
+  exit 1                                        
+fi                                              
 
 # check if CAPI boards exists
 capi_check=`lspci -d "1014":"477" | wc -l`
-if [ $capi_check -eq 0 ]; then
+if [ $capi_check -eq 0 ]; then            
   printf "${bold}ERROR:${normal} No CAPI devices found\n"
-  exit 1
-fi
+  exit 1                                                 
+fi                                                       
 
 # make cxl dir if not present
-mkdir -p /var/cxl/
+mkdir -p /var/cxl/           
 
 # mutual exclusion
-if ! mkdir /var/cxl/capi-flash-script.lock 2>/dev/null; then
+if [ ! -d "/var/cxl/capi-flash-script.lock" ]; then
+  mkdir /var/cxl/capi-flash-script.lock            
+else                                               
   printf "${bold}ERROR:${normal} Another instance of this script is running\n"
-  exit 1
-fi
-trap 'rm -rf "/var/cxl/capi-flash-script.lock"' 0
+  exit 1                                                                      
+fi                                                                            
+#if ! mkdir /var/cxl/capi-flash-script.lock 2>/dev/null; then                 
+#  printf "${bold}ERROR:${normal} Another instance of this script is running\n"
+#  exit 1                                                                      
+#fi                                                                            
+trap 'rm -rf "/var/cxl/capi-flash-script.lock"' 0                              
 
 # get pwd
 pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -122,12 +134,12 @@ pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 n=`ls -d /sys/class/cxl/card* | awk -F"/sys/class/cxl/card" '{ print $2 }' | wc -w`
 
 # touch history files if not present
-for i in `seq 0 $(($n - 1))`; do
-  f="/var/cxl/card$i"
-  if [[ ! -f $f ]]; then
-    touch $f
-  fi
-done
+for i in `seq 0 $(($n - 1))`; do    
+  f="/var/cxl/card$i"               
+  if [[ ! -f $f ]]; then            
+    touch $f                        
+  fi                                
+done                                
 
 # print current date on server for comparison
 printf "\n${bold}Current date:${normal}\n$(date)\n\n"
@@ -136,43 +148,43 @@ printf "\n${bold}Current date:${normal}\n$(date)\n\n"
 printf "${bold}%-7s %-30s %-29s %-20s %s${normal}\n" "#" "Card" "Flashed" "by" "Image"
 
 # print card information and flash history
-i=0;
-while read d; do
+i=0;                                      
+while read d; do                          
   p[$i]=$(cat /sys/class/cxl/card$i/device/subsystem_device)
-  # check for legacy device
-  if [[ ${p[$i]:0:6} == "0x04af" ]]; then
+  # check for legacy device                                 
+  if [[ ${p[$i]:0:6} == "0x04af" ]]; then                   
     p[$i]=$(cat /sys/class/cxl/card$i/psl_revision | xargs printf "0x%.4X")
-  fi
-  f=$(cat /var/cxl/card$i)
-  while IFS='' read -r line || [[ -n $line ]]; do
-    if [[ ${line:0:6} == ${p[$i]:0:6} ]]; then
-      parse_info=($line)
-      board_vendor[$i]=${parse_info[1]}
-      fpga_type[$i]=${parse_info[2]}
+  fi                                                                       
+  f=$(cat /var/cxl/card$i)                                                 
+  while IFS='' read -r line || [[ -n $line ]]; do                          
+    if [[ ${line:0:6} == ${p[$i]:0:6} ]]; then                             
+      parse_info=($line)                                                   
+      board_vendor[$i]=${parse_info[1]}                                    
+      fpga_type[$i]=${parse_info[2]}                                       
       printf "%-7s %-30s %-29s %-20s %s\n" "card$i" "${line:6}" "${f:0:29}" "${f:30:20}" "${f:51}"
-    fi
-  done < "$pwd/../capi-utils/psl-devices"
-  i=$[$i+1]
-done < <(lspci -d "1014":"477" )
+    fi                                                                                            
+  done < "$pwd/../capi-utils/psl-devices"                                                         
+  i=$[$i+1]                                                                                       
+done < <(lspci -d "1014":"477" )                                                                  
 
 printf "\n"
 
 # card is set via parameter since it is positive
-if (($card >= 0)); then
-  c=$((10#$card))
-  if (( "$c" >= "$n" )); then
+if (($card >= 0)); then                         
+  c=$((10#$card))                               
+  if (( "$c" >= "$n" )); then                   
     printf "${bold}ERROR:${normal} Wrong card number ${card}\n"
-    exit 1
-  fi
-else
-# prompt card to flash to
-  while true; do
+    exit 1                                                     
+  fi                                                           
+else                                                           
+# prompt card to flash to                                      
+  while true; do                                               
     read -p "Which card do you want to flash? [0-$(($n - 1))] " c
-    if ! [[ $c =~ ^[0-9]+$ ]]; then
-      printf "${bold}ERROR:${normal} Invalid input\n"
-    else
-      c=$((10#$c))
-      if (( "$c" >= "$n" )); then
+    if ! [[ $c =~ ^[0-9]+$ ]]; then                              
+      printf "${bold}ERROR:${normal} Invalid input\n"            
+    else                                                         
+      c=$((10#$c))                                               
+      if (( "$c" >= "$n" )); then                                
         printf "${bold}ERROR:${normal} Wrong card number\n"
         exit 1
       else
@@ -184,19 +196,6 @@ fi
 
 printf "\n"
 
-# check file type
-FILE_EXT=${1##*.}
-if [[ ${fpga_type[$c]} == "Altera" ]]; then
-  if [[ $FILE_EXT != "rbf" ]]; then
-    printf "${bold}ERROR: ${normal} Incorrect board type (Altera/Xilinx)\n"
-    exit 0
-  fi
-else
-  if [[ $FILE_EXT != "bin" ]]; then
-    printf "${bold}ERROR: ${normal} Incorrect board type (Altera/Xilinx)\n"
-    exit 0
-  fi
-fi
 
 # card is set via parameter since it is positive
 if (($force != 1)); then
@@ -262,3 +261,5 @@ fi
 printf "\nMake sure to use ${bold}/dev/cxl/afu$c.0d${normal} in your host application;\n\n"
 printf "#define DEVICE \"/dev/cxl/afu$c.0d\"\n"
 printf "struct cxl_afu_h *afu = cxl_afu_open_dev ((char*) (DEVICE));\n\n"
+
+rm -rf "/var/cxl/capi-flash-script.lock
